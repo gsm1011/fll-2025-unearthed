cb7cffb4ce2e85a3275bce6eac6bd776bbd035c5
Shumin Guo <gsmsteve@gmail.com>
2025-12-02 14:27:12 -0800

    New implementation for the missions.

--- m7-5-6-9.py
+++ m7-5-6-9.py
@@ -13,10 +13,39 @@ def Run(br: BaseRobot):
     br.hub.display.number(7)
     br.hub.speaker.beep()
 
+    br.driveForDistance(distance=650, speedPct=60)
+    br.turnRightInPlace(angle=45, speedPct=50)
+    br.driveForward(distance=30, speedPct=50)
+    br.lowerLeftArm(degrees=90, speedPct=20)
+    br.driveForward(distance=55, speedPct=50)
+    br.raiseLeftArm(degrees=120, speedPct=20)
     
-    br.driveForDistance(distance=550, speedPct=60)
+    br.hub.display.number(6)
+    br.turnLeftInPlace(angle=25, speedPct=50)
+    
+    br.hub.display.number(5)
+    br.driveBackward(distance=100, speedPct=50)
+    br.turnLeftInPlace(angle=15, speedPct=50)
+    br.driveForward(distance=150, speedPct=50)
+    br.turnLeftInPlace(angle=45, speedPct=50)
+    
+    br.hub.display.number(9)
+    br.driveBackward(distance=50, speedPct=60)
+    br.turnLeftInPlace(angle=55, speedPct=50)
+    br.driveForward(distance=380, speedPct=60)
+    br.turnRightInPlace(angle=25, speedPct=50)
+    br.driveBackward(distance=250, speedPct=60)
+    
+    # go home.
+    br.driveForward(distance=100, speedPct=60)
+    br.turnLeftInPlace(angle=30, speedPct=50)
+    br.driveBackward(distance=200, speedPct=60)
+    br.turnRightInPlace(angle=35, speedPct=50)
+    br.driveBackward(distance=700, speedPct=100)
+    
+    return
     br.turnInPlace(angle=50, speedPct=50)
-    br.driveForDistance(distance=200, speedPct=50)
+    br.driveForDistance(distance=100, speedPct=50)
     br.turnInPlace(angle=-90, speedPct=50)
     br.driveForDistance(distance=-70, speedPct=60)
     
