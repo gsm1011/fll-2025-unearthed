2bc35daaca91fe8cc8c138b4a01411f5176d20eb
Shumin Guo <gsmsteve@gmail.com>
2025-12-02 16:42:40 -0800

    some updates on the arm starting place.

--- m2.py
+++ m2.py
@@ -14,28 +14,26 @@ def Run(br: BaseRobot):
     br.hub.speaker.beep()
 
     # br.moveLeftAttachmentMotorForDegrees(degrees=55, speedPct=30)
-    br.driveForDistance(distance=680, speedPct=60)
-    br.turnInPlace(angle=-45, speedPct=50)
-    br.driveForDistance(distance=-50, speedPct=100)
-    br.moveLeftAttachmentMotorForDegrees(degrees=55, speedPct=20)
-    wait(1000)
-    br.driveForDistance(distance=60, speedPct=100)
-    br.moveLeftAttachmentMotorForDegrees(degrees=-75, speedPct=20)
+    br.driveForward(distance=690, speedPct=60)
+    br.turnLeftInPlace(angle=45, speedPct=50)
+    br.driveBackward(distance=50, speedPct=100)
+    # br.lowerLeftArm(degrees=55, speedPct=20)
+    wait(500)
+    br.driveForward(distance=80, speedPct=100)
+    br.raiseLeftArm(degrees=75, speedPct=20)
     
     # push soil back
-    br.driveForDistance(distance=200, speedPct=60)
+    br.driveForward(distance=220, speedPct=60)
     
     # br.turnInPlace(angle=-45, speedPct=50)
-    br.driveForDistance(distance=-200, speedPct=100)
+    br.driveBackward(distance=200, speedPct=100)
     # br.moveLeftAttachmentMotorForDegrees(degrees=-55, speedPct=30)
     
     # drive home
     # br.driveForDistance(distance=-100, speedPct=100)
-    br.turnInPlace(angle=55, speedPct=50)
-    br.driveForDistance(distance=-750, speedPct=60)
+    br.turnRightInPlace(angle=55, speedPct=100)
+    br.driveBackward(distance=750, speedPct=100)
     
-    wait(1000)
-        
 if __name__ == "__main__":
     br = BaseRobot()
     Run(br)
