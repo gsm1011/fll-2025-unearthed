41c064b6869910c6a02ad573cec0e12a6ca09ef1
Shumin Guo <gsmsteve@gmail.com>
2025-12-04 15:41:11 -0800

    Updates.

--- m1_3_13.py
+++ m1_3_13.py
@@ -13,33 +13,35 @@ def Run(br: BaseRobot):
     br.hub.display.number(1)
     br.hub.speaker.beep()
 
-    br.driveForDistance(distance=820, speedPct=60)
-    br.turnInPlace(angle=90, speedPct=50)
+    br.driveForward(distance=820, speedPct=60)
+    br.turnRightInPlace(angle=90, speedPct=50)
     
     br.hub.display.number(3)
-    br.moveLeftAttachmentMotorForDegrees(degrees=120, speedPct=30)
-    br.driveForDistance(distance=200, speedPct=60)
+    br.lowerLeftArm(degrees=120, speedPct=30)
+    br.driveForward(distance=200, speedPct=60)
     
-    # do the lift
-    br.moveLeftAttachmentMotorForDegrees(degrees=-120, speedPct=100)
+    # lift the cart
+    br.raiseLeftArm(degrees=150, speedPct=30)
     
     # back and go for mission 13
     br.hub.display.number(13)
-    br.driveForDistance(distance=-200, speedPct=60)
+    br.driveBackward(distance=200, speedPct=60)
     
-    br.turnInPlace(angle=25, speedPct=50)
-    br.driveForDistance(distance=250, speedPct=60)
-    br.moveLeftAttachmentMotorForDegrees(degrees=120, speedPct=20)
-    br.driveForDistance(distance=50, speedPct=60)
+    br.turnRightInPlace(angle=28, speedPct=50)
+    br.driveForward(distance=250, speedPct=60)
+    br.lowerLeftArm(degrees=120, speedPct=20)
+    br.driveForward(distance=50, speedPct=60)
     
     # lift the statue
-    br.moveLeftAttachmentMotorForDegrees(degrees=-90, speedPct=30)
+    br.raiseLeftArm(degrees=90, speedPct=30)
     
     # back home
-    br.turnInPlace(angle=-25, speedPct=50)
-    br.driveForDistance(distance=-300, speedPct=60)
-    br.turnInPlace(angle=-90, speedPct=50)
-    br.driveForDistance(distance=-700, speedPct=60)
+    br.hub.display.char('H')
+    br.turnLeftInPlace(angle=23, speedPct=50)
+    # br.curve(radius=350, angle=-120, speedPct=50)
+    br.driveBackward(distance=300, speedPct=60)
+    br.turnLeftInPlace(angle=90, speedPct=50)
+    br.driveBackward(distance=700, speedPct=60)
     
     return
     wait(1000)
