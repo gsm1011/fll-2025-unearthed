257c700400f7ed50a6eeeb249fdde49f105bf381
Shumin Guo <gsmsteve@gmail.com>
2025-12-06 13:35:29 -0800

    Removed mission 9 due to reliability problem

--- m7_6_5_9.py
+++ m7_6_5_9.py
@@ -29,26 +29,26 @@ def Run(br: BaseRobot):
     
     br.hub.display.number(5)
     br.driveBackward(distance=100, speedPct=50)
-    br.turnLeftInPlace(angle=22, speedPct=50)
+    br.turnLeftInPlace(angle=20, speedPct=50)
     br.driveForward(distance=138, speedPct=50)
     
-    br.turnLeftInPlace(angle=31, speedPct=50)
-    br.turnRightInPlace(angle=10, speedPct=50)
+    br.turnLeftInPlace(angle=40, speedPct=50)
+    br.turnRightInPlace(angle=20, speedPct=50)
     
-    br.hub.display.number(9)
-    br.driveBackward(distance=50, speedPct=60)
-    br.turnLeftInPlace(angle=52, speedPct=50)
+    # br.hub.display.number(9)
+    # br.driveBackward(distance=50, speedPct=60)
+    # br.turnLeftInPlace(angle=52, speedPct=50)
     
-    br.driveForward(distance=440, speedPct=60)
-    br.turnRightInPlace(angle=17, speedPct=50)
-    br.driveBackward(distance=300, speedPct=50)
+    # br.driveForward(distance=440, speedPct=60)
+    # br.turnRightInPlace(angle=23, speedPct=50)
+    # br.driveBackward(distance=300, speedPct=50)
     
     # go home.
     br.hub.display.char('H')
-    br.driveForward(distance=150, speedPct=60)
-    br.turnLeftInPlace(angle=35, speedPct=50)
-    br.curve(radius=-620, angle=-70, speedPct=150)
-    br.driveBackward(distance=300, speedPct=60)
+    br.driveBackward(distance=750, speedPct=60)
+    # br.turnLeftInPlace(angle=45, speedPct=50)
+    # # br.curve(radius=-620, angle=-70, speedPct=150)
+    # br.driveBackward(distance=600, speedPct=60)
         
 if __name__ == "__main__":
     br = BaseRobot()
